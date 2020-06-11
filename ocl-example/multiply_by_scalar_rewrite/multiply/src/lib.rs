b77f87a73822f5f394d9e0d7f359456f4a0850cd
costa-wang <3162284013@qq.com>
2020-06-11 13:02:01 +0800

    multiply_rewrite

--- ocl-example/multiply_by_scalar_rewrite/multiply/src/lib.rs
+++ ocl-example/multiply_by_scalar_rewrite/multiply/src/lib.rs
@@ -1,5 +1,4 @@
 extern crate ocl;
-extern crate ocl_extras;
 extern crate log;
 
 use std::result::Result;
@@ -8,20 +7,16 @@ use log::info;
 
 static MULTIPLY_SRC: &str = include_str!("kernel/multiply.cl");
 
-// pub fn hello(){
-//     println!("{}",MULTIPLY_SRC);
-// }
-
 pub struct MultiplyKernel
 {
     proque: ProQue,
     source_buffer: Buffer<f32>,
-    result_buffer: Buffer<f32>,
+    pub result_buffer: Buffer<f32>
 }
 
 impl MultiplyKernel
 {
-    pub fn create(work_size: usize) -> Result<MultiplyKernel,Error> {
+    pub fn create(work_size: usize,vec_source: &Vec<f32>) -> Result<MultiplyKernel,Error> {
         // let devices = &GPU_NVIDIA_DEVICES;
         // if devices.is_empty() {
         //     return Err(GPUError::Simple("No working GPUs found!"));
@@ -29,17 +24,14 @@ impl MultiplyKernel
         // let device = devices[0]; // Select the first device for FFT
         let pq = ProQue::builder().src(MULTIPLY_SRC).dims(work_size).build()?;
 
-
-        let vec_source = ocl_extras::scrambled_vec((0.0, 20.0), pq.dims().to_len());
         
         let source_buffer = Buffer::builder()
         .queue(pq.queue().clone())
         .flags(MemFlags::new().read_write())
         .len(work_size)
-        .copy_host_slice(&vec_source)
+        .copy_host_slice(vec_source)
         .build()?;
 
-        // let mut vec_result = vec![0.0f32; work_size];
         let result_buffer: Buffer<f32> = pq.create_buffer()?;
 
         info!("FFT: 1 working device(s) selected.");
@@ -51,12 +43,24 @@ impl MultiplyKernel
             result_buffer: result_buffer,
         })
     }
-}
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    pub fn multiply(&mut self,work_size: usize,coeff:f32){
+        let kern = self.proque.kernel_builder("multiply_by_scalar")
+        .arg(coeff)
+        .arg(None::<&Buffer<f32>>)
+        .arg_named("result", None::<&Buffer<f32>>)
+        .build().unwrap();
+        
+        // Set our named argument. The Option<_> wrapper is, well... optional:
+        kern.set_arg("result", &self.result_buffer);
+        // We can also set arguments (named or not) by index. Just for
+        // demonstration, we'll set one using an option:
+        kern.set_arg(0, &coeff);
+        kern.set_arg(1, Some(&self.source_buffer));
+        kern.set_arg(2, &self.result_buffer);
+        
+        unsafe { kern.enq();}
+        // Read results from the device into result_buffer's local vector:
     }
 }
+
--- ocl-example/multiply_by_scalar_rewrite/src/main.rs
+++ ocl-example/multiply_by_scalar_rewrite/src/main.rs
@@ -1,4 +1,6 @@
+extern crate ocl_extras;
 use multiply::{ MultiplyKernel};
+
 
 const RESULTS_TO_PRINT: usize = 20;
 
@@ -7,5 +9,18 @@ const WORK_SIZE: usize = 1 << 20;
 const COEFF: f32 = 5432.1;
 
 fn main() {
-    MultiplyKernel::create(WORK_SIZE);
+    let vec_source = ocl_extras::scrambled_vec((0.0, 20.0), WORK_SIZE);
+    let mut kernel = MultiplyKernel::create(WORK_SIZE,&vec_source).unwrap();
+    kernel.multiply(WORK_SIZE,COEFF);
+
+    let mut vec_result = vec![0.0f32; WORK_SIZE];
+    kernel.result_buffer.read(&mut vec_result).enq();
+
+    for idx in 0..WORK_SIZE {
+        if idx < RESULTS_TO_PRINT {
+            println!("source[{idx}]: {:.03}, \t coeff: {}, \tresult[{idx}]: {}",
+            vec_source[idx], COEFF, vec_result[idx], idx = idx);
+        }
+        assert_eq!(vec_source[idx] * COEFF, vec_result[idx]);
+    }
 }
